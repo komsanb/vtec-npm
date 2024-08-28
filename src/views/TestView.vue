feb956615247f4964596672c1cf3c2bea95ca693
Komsan Bureephakdee <tengnueng.kb@gmail.com>
2024-08-28 17:05:10 +0700

    Update src

---
+++ src/views/TestView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <h1>This is an test page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
